0e6e977a7f0e73a3f78e81f50f5696987ae4b24a
kendrang <kendrang@users.noreply.github.com>
2019-12-13 14:46:46 -0500

    first

---
+++ src/main/java/com/zipcode/group3blog/group3blog/Group3blogApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.group3blog.group3blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group3blogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group3blogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zipcode/group3blog/group3blog/Group3blogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.group3blog.group3blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group3blogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
