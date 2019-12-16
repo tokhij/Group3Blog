8bbd9a53c41ed5671534999a4fc43cff0845fccd
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-12-16 14:14:42 -0500

    Fix actuator endpoint

--- src/main/java/com/zipcode/group3blog/configuration/SecurityConfig.java
+++ src/main/java/com/zipcode/group3blog/configuration/SecurityConfig.java
@@ -41,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/api/posts/all")
                 .permitAll()
+                .antMatchers("/actuator/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
 
