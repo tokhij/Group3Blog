0aecf3d9d53465ac6d0737d12578a9ee167ce4b1
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-16 15:35:25 -0500

    working on comments

--- src/main/java/com/zipcode/group3blog/controller/CommentController.java
+++ src/main/java/com/zipcode/group3blog/controller/CommentController.java
@@ -1,25 +1,42 @@
 package com.zipcode.group3blog.controller;
 
-import com.zipcode.group3blog.dto.CommentDto;
+import com.zipcode.group3blog.dto.CommentDTO;
+import com.zipcode.group3blog.dto.PostDTO;
 import com.zipcode.group3blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CommentController {
-    // add this into parameter of get/ post mapping- /{postId}/comments
+    // add this into parameter of get/post mapping- /api/{postId}/comments
     @Autowired
     private CommentService commentService;
 
     @PostMapping
-    public ResponseEntity createComment(@RequestBody CommentDto commentDto){
-        commentService.createComment(commentDto);
+    public ResponseEntity createComment(@RequestBody CommentDTO commentDTO){
+        commentService.createComment(commentDTO);
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
+    @GetMapping("/{postId}/comments")
+    public ResponseEntity<List<CommentDTO>> showAllComments() {
+        return new ResponseEntity<>(commentService.showAllComments(), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}/comments")
+    public ResponseEntity<CommentDTO> getSingleComment(@PathVariable @RequestBody Long id) {
+        return new ResponseEntity<>(commentService.readSingleComment(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/get/{id}/comments")
+    public ResponseEntity<Boolean> deletePost(@PathVariable @RequestBody CommentDTO commentDTO) {
+        commentService.deleteComment(commentDTO);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
